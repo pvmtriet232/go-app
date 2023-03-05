8a2626112e9e5511792ea5236715a2c470608e1b
triet <pvmtriet232@gmail.com>
2023-03-05 18:41:58 +0700

    fix again

--- hello/hello.go
+++ hello/hello.go
@@ -4,13 +4,11 @@ import (
 	"fmt"
 
 	"github.com/pvmtriet232/go-app/greetings"
-	_ "github.com/pvmtriet232/go-app/people"
 )
 
 func main() {
 	// Get a greeting message and print it.
 	message := greetings.Hello("Gladys")
 	fmt.Println(message)
-	Ann := People{"Ann"}
-	fmt.Println(Ann)
+
 }
