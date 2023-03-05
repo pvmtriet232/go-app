9220c672480d792384135fce65d3ce485f5a5af6
triet <pvmtriet232@gmail.com>
2023-03-05 18:18:07 +0700

    changing import

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/pvmtriet233/go-app/greetings"
 )
 
 func main() {
-	// get a greeting message and print it.
+	// Get a greeting message and print it.
 	message := greetings.Hello("Gladys")
 	fmt.Println(message)
-
 }
